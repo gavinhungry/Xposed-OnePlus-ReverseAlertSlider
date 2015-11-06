3588171e1dea96c960cd75b46c54350ea0836a40
Gavin Lloyd <gavinhungry@gmail.com>
2015-11-05 19:24:34 -0800

    Hide zen pane

--- src/io/gavinhungry/xposed/oneplus/reversealertslider/ReverseAlertSlider.java
+++ src/io/gavinhungry/xposed/oneplus/reversealertslider/ReverseAlertSlider.java
@@ -7,13 +7,11 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 
+
 public class ReverseAlertSlider implements IXposedHookLoadPackage {
   public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
     if (lpparam.packageName.equals("com.android.systemui")) {
       try {
-
-        XposedHelpers.findAndHookMethod("com.android.systemui.volume.ZenToast", lpparam.classLoader,
-          "handleShow", int.class, String.class, XC_MethodReplacement.DO_NOTHING);
 
         XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.policy.ZenModeControllerImpl", lpparam.classLoader,
           "setZen", int.class, new XC_MethodHook() {
@@ -23,13 +21,30 @@ public class ReverseAlertSlider implements IXposedHookLoadPackage {
             // 1 - No interruptions
             // 2 - Priority interruptions only
             // 3 - All notifications
-            if (param.args[0].equals(1)) {
+            int state = (Integer) param.args[0];
+
+            if (state == 1) {
               param.args[0] = 3;
-            } else if (param.args[0].equals(3)) {
+            } else if (state == 3) {
               param.args[0] = 1;
             }
           }
         });
+
+        XposedHelpers.findAndHookMethod("com.android.systemui.volume.ZenModePanel", lpparam.classLoader,
+          "handleUpdateZen", int.class, XC_MethodReplacement.DO_NOTHING);
+
+        XposedHelpers.findAndHookMethod("com.android.systemui.volume.VolumePanel", lpparam.classLoader,
+          "setZenPanelVisible", boolean.class, new XC_MethodHook() {
+
+          @Override
+          protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+            param.args[0] = false;
+          }
+        });
+
+        XposedHelpers.findAndHookMethod("com.android.systemui.volume.ZenToast", lpparam.classLoader,
+          "handleShow", int.class, String.class, XC_MethodReplacement.DO_NOTHING);
 
       } catch(Throwable t) {
         XposedBridge.log(t);
